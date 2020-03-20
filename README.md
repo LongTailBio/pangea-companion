bb828b2853013458698f3e2d637eb8e4dd82301d
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2020-03-20 08:52:01 -0400

    feat: add Uppy companion app

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Pangea Companion
 
 Uppy Companion server for handling S3 uploads.
+
+## Getting Started
+
+**Configure**
+
+```sh
+cp .env.example .env
+```
+
+**Run**
+
+```sh
+yarn start
+```
+
+## Deploying
+
+Pangea Companion expects to be run behind a SSL-terminating proxy in production.
---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const companion = require("@uppy/companion");
+const winston = require("winston");
+const envalid = require("envalid");
+const { str, url, port } = envalid;
+
+const logger = winston.createLogger({
+  exitOnError: false,
+  transports: [
+    new winston.transports.Console({
+      level: "info",
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+      )
+    })
+  ]
+});
+
+const env = envalid.cleanEnv(process.env, {
+  PORT: port({ default: 3020 }),
+  SESSION_SECRET: str({ desc: "Secure session secret" }),
+  COMPANION_FILE_PATH: str(),
+  AWS_KEY: str(),
+  AWS_SECRET: str(),
+  AWS_REGION: str({ default: "us-east-1" }),
+  AWS_S3_ENDPOINT: url({ default: undefined }),
+  AWS_S3_BUCKET: str()
+});
+
+var app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const sess = {
+  secret: env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {}
+};
+
+if (env.isProd) {
+  app.set("trust proxy", 1); // trust first proxy
+  sess.cookie.secure = true; // serve secure cookies
+}
+
+app.use(session(sess));
+
+// Companion
+const options = {
+  providerOptions: {
+    s3: {
+      key: env.AWS_KEY,
+      secret: env.AWS_SECRET,
+      bucket: env.AWS_S3_BUCKET,
+      region: env.AWS_REGION,
+      awsClientOptions: env.AWS_S3_ENDPOINT
+        ? {
+            endpoint: env.AWS_S3_ENDPOINT
+          }
+        : {},
+      getKey: (req, filename, metadata) => {
+        logger.info("Getting key", { filename, metadata });
+        return filename;
+      },
+      acl: "private"
+    }
+  },
+  server: {
+    host: `127.0.0.1:${env.PORT}`,
+    protocol: "http"
+  },
+  filePath: env.COMPANION_FILE_PATH
+};
+
+app.use(companion.app(options));
+
+const server = app.listen(env.PORT, "127.0.0.1", () => {
+  const host = server.address().address;
+  const port = server.address().port;
+  logger.info(`Pangea Companion listening at http://${host}:${port}`);
+});
+
+companion.socket(server, options);
