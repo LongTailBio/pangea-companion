307d64f29ebb9953523ea6bf57e325a710393e29
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2020-03-21 20:19:38 -0400

    feat: add pangea auth (#1)
    Require `X-Pangea-Token` header. Use this to authenticate Pangea user.

--- index.js
+++ index.js
@@ -1,86 +1,38 @@
+const companion = require("@uppy/companion");
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const session = require("express-session");
-const companion = require("@uppy/companion");
-const winston = require("winston");
-const envalid = require("envalid");
-const { str, url, port } = envalid;
 
-const logger = winston.createLogger({
-  exitOnError: false,
-  transports: [
-    new winston.transports.Console({
-      level: "info",
-      format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.json()
-      )
-    })
-  ]
-});
+const config = require("./lib/config");
+const logger = require("./lib/logger");
+const pangeaAuth = require("./lib/auth");
+const options = require("./lib/companion");
+const { UnknownContribModuleError } = require("./lib/errors");
 
-const env = envalid.cleanEnv(process.env, {
-  PORT: port({ default: 3020 }),
-  SESSION_SECRET: str({ desc: "Secure session secret" }),
-  COMPANION_FILE_PATH: str(),
-  AWS_KEY: str(),
-  AWS_SECRET: str(),
-  AWS_REGION: str({ default: "us-east-1" }),
-  AWS_S3_ENDPOINT: url({ default: undefined }),
-  AWS_S3_BUCKET: str()
-});
+const companionApp = companion.app(options);
 
-var app = express();
-app.use(cors());
+const app = express();
 app.use(bodyParser.json());
+app.use(
+  cors({
+    preflightContinue: true,
+    allowedHeaders: ["content-type", "x-pangea-token"]
+  })
+);
 
-const sess = {
-  secret: env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true,
-  cookie: {}
-};
+app.use(pangeaAuth);
+app.use(companionApp);
 
-if (env.isProd) {
-  app.set("trust proxy", 1); // trust first proxy
-  sess.cookie.secure = true; // serve secure cookies
-}
+app.use((err, req, res, next) => {
+  if (!res.headersSent && err instanceof UnknownContribModuleError) {
+    logger.info("unknown_contrib_module", { module_name: err.moduleName });
+    res.status(400);
+    return res.send(err.message);
+  }
+  return next(err);
+});
 
-app.use(session(sess));
-
-// Companion
-const options = {
-  providerOptions: {
-    s3: {
-      key: env.AWS_KEY,
-      secret: env.AWS_SECRET,
-      bucket: env.AWS_S3_BUCKET,
-      region: env.AWS_REGION,
-      awsClientOptions: env.AWS_S3_ENDPOINT
-        ? {
-            endpoint: env.AWS_S3_ENDPOINT
-          }
-        : {},
-      getKey: (req, filename, metadata) => {
-        const { userId } = metadata;
-        logger.info("Getting key", { filename, metadata });
-        if (!userId) throw new Error("Must provide userId!");
-        return `covid19/user-${userId}`;
-      },
-      acl: "private"
-    }
-  },
-  server: {
-    host: `127.0.0.1:${env.PORT}`,
-    protocol: "http"
-  },
-  filePath: env.COMPANION_FILE_PATH
-};
-
-app.use(companion.app(options));
-
-const server = app.listen(env.PORT, "127.0.0.1", () => {
+const server = app.listen(config.PORT, config.HOST, () => {
   const host = server.address().address;
   const port = server.address().port;
   logger.info(`Pangea Companion listening at http://${host}:${port}`);
